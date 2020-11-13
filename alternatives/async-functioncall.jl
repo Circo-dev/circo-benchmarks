461f8d9911a935e2c43be77c5ab1713c8fc6a513
Schaffer Krisztian <schaffer.krisztian@gmail.com>
2020-11-13 15:33:40 +0100

    async-functioncall added

---
+++ alternatives/async-functioncall.jl
@@ -0,0 +1,38 @@
+using BenchmarkTools
+import Base.Threads.@spawn # Seems it is the same as @async when "--threads 1"
+
+function channelize(calcfn)
+    return (input, output)->begin
+        while true
+            data = take!(input)
+            put!(output, calcfn(data))
+        end
+    end
+end
+
+function createcalc(channel_length = 1)
+    source, target = (Channel{Int64}(channel_length) for i = 1:2)
+    @spawn channelize(x -> 2x)(source, target)
+    return source, target
+end
+
+function runcalc(source, target, N = 1_000_000)
+    @spawn begin
+        for i = 1:N
+            put!(source, i)
+        end
+    end
+    @spawn begin
+        sum = 0
+        for i = 1:N
+            sum += take!(target)
+        end
+        return sum
+    end
+end
+
+for channel_length in (0, 1, 2, 4, 8, 16, Inf)
+    @info "---"
+    @info "Channel length: $channel_length"
+    @btime fetch(runcalc(params...)) setup = (params = createcalc($channel_length))
+end
