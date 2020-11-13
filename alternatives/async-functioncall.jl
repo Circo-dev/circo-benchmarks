514ad52962bc4a51c11b9e7cd21a2103dfcab534
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2020-11-13 16:02:10 +0100

    @async added to async-functioncall

--- alternatives/async-functioncall.jl
+++ alternatives/async-functioncall.jl
@@ -1,6 +1,9 @@
 using BenchmarkTools
 import Base.Threads.@spawn # Seems it is the same as @async when "--threads 1"
 
+const N = 10
+const fn = x -> 2x
+
 function channelize(calcfn)
     return (input, output)->begin
         while true
@@ -12,11 +15,11 @@ end
 
 function createcalc(channel_length = 1)
     source, target = (Channel{Int64}(channel_length) for i = 1:2)
-    @spawn channelize(x -> 2x)(source, target)
+    @spawn channelize(fn)(source, target)
     return source, target
 end
 
-function runcalc(source, target, N = 1_000_000)
+function runcalc(source, target; N = N)
     @spawn begin
         for i = 1:N
             put!(source, i)
@@ -30,6 +33,17 @@ function runcalc(source, target, N = 1_000_000)
         return sum
     end
 end
+
+function runasync(fn; N = N)
+    sum = 0
+    for i = 1:N
+        sum += fetch(@async fn(i))
+    end
+    return sum
+end
+
+@info "@async:"
+@btime runasync(fn)
 
 for channel_length in (0, 1, 2, 4, 8, 16, Inf)
     @info "---"
